d794bc717be910bca6d3e8f7be4204d964f6509c
Francesca Villalva <pilar.villalva@gmail.com>
2022-03-02 16:14:20 -0500

    add challenge solution

---
+++ 8kyu/removeStringSpaces.js
@@ -0,0 +1,10 @@
+/*
+8kyu_remove string spaces
+24 Mar 2022
+
+Simple, remove the spaces from the string, then return the resultant string.
+*/
+
+function noSpace(x){
+    return x.split(' ').join('')
+}
