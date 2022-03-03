2b3915fcb1ac6e3956d6cc54409ab420a26a832c
Francesca Villalva <pilar.villalva@gmail.com>
2022-03-03 16:36:51 -0500

    add challenge solution

---
+++ 8kyu/totalAmountOfPoints.js
@@ -0,0 +1,33 @@
+/*
+8kyu total amount of points
+29 march 2022
+
+Our football team finished the championship. The result of each match look like "x:y". 
+Results of all matches are recorded in the collection.
+
+Ex: ["3:1", "2:2", "0:1", ...]
+
+Write a function that takes such collection and counts the points of our team in the championship. 
+Rules for counting points for each match:
+if x > y: 3 points
+if x < y: 0 point
+if x = y: 1 point
+
+Notes: there are 10 matches in the championship
+0 <= x <= 4
+0 <= y <= 4
+*/
+
+function points(games) {
+    let points = 0
+    for (let i = 0; i < games.length; i++) {
+      if (games[i][0] > games[i][2]) {
+        points += 3
+      } else if (games[i][0] === games[i][2]) {
+        points += 1
+      } else {
+        points += 0
+      }
+    }
+    return points
+}
