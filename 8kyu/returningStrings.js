23dcdad27fdc9f910beb811f9042f240c36713c4
Francesca Villalva <pilar.villalva@gmail.com>
2022-03-04 16:10:25 -0500

    add challenge solution

---
+++ 8kyu/returningStrings.js
@@ -0,0 +1,11 @@
+/*
+8kyu_returning strings
+27 march 2022
+
+Make a function that will return a greeting statement that uses an input
+Your program should return, "Hello, <name> how are you doing today?"
+*/
+
+function greet(name){
+    return `Hello, ${name} how are you doing today?`
+}
