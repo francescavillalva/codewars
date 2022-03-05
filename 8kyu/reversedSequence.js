29eb345bb626c3906404a596892fba191f2101a9
Francesca Villalva <pilar.villalva@gmail.com>
2022-03-05 16:09:52 -0500

    add challenge solution

---
+++ 8kyu/reversedSequence.js
@@ -0,0 +1,16 @@
+/*
+8 kyu reversed sequence
+21 Mar 2022
+
+Build a function that returns an array of integers from n to 1 where n>0.
+
+Ex : n=5 --> [5,4,3,2,1]
+*/
+
+const reverseSeq = n => {
+    let array = []
+    for (let i = n; i > 0; i--) {
+      array.push(i)
+    }
+    return array
+  }
