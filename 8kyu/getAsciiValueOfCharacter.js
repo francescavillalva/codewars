581b5e0b5cd01dc002bc6e25bd2d929803677cc6
Francesca Villalva <pilar.villalva@gmail.com>
2022-03-09 15:01:55 -0500

    add  challenge solution

---
+++ 8kyu/getAsciiValueOfCharacter.js
@@ -0,0 +1,12 @@
+/*
+8lyu get ascii value of character
+11 may 2022
+
+Get ASCII value of a character.
+
+For the ASCII table you can refer to http://www.asciitable.com/
+*/
+
+function getASCII(c){
+    return c.charCodeAt()
+}
