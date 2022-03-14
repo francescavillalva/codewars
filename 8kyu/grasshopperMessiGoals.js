013edeb51b2e55aaacf15d0f07078ea116711d08
Francesca Villalva <pilar.villalva@gmail.com>
2022-03-14 16:58:39 -0400

    add challenge

---
+++ 8kyu/grasshopperMessiGoals.js
@@ -0,0 +1,24 @@
+/*
+8kyu_grasshopper messi goals
+1 april 2022
+
+Use variables to find the sum of the goals Messi scored in 3 competitions
+
+Competition	Goals
+La Liga	43
+Champions League	10
+Copa del Rey	5
+
+Create these three variables and store the appropriate values using the table above:
+laLigaGoals
+championsLeagueGoals
+copaDelReyGoals
+
+Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
+*/
+
+var laLigaGoals = 43
+var championsLeagueGoals = 10
+var copaDelReyGoals = 5
+
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
