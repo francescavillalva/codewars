2bf0ce53629d2606853a444b21a8c41921df0a55
Francesca Villalva <pilar.villalva@gmail.com>
2022-04-02 01:29:11 -0400

    add an 8kyu challenge

---
+++ 8kyu/basicMathematicalOperations.js
@@ -0,0 +1,36 @@
+/*
+8kyu_basic mathematical operations
+27 march 2022
+
+Your task is to create a function that does four basic mathematical operations.
+
+The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
+
+Ex:
+(Operator, value1, value2) --> output
+('+', 4, 7) --> 11
+('-', 15, 18) --> -3
+('*', 5, 5) --> 25
+('/', 49, 7) --> 7
+*/
+
+function basicOp(operation, value1, value2) {
+    let result = 0
+    
+    switch (operation) {
+        case '+':
+          result = value1 + value2
+          break
+        case '-':
+          result = value1 - value2
+          break
+        case '*':
+          result = value1 * value2
+          break
+        case '/':
+          result = value1 / value2
+          break
+    }
+    return result
+}
