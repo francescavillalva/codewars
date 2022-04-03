aea39ac702bdbd76da3a03fa94b9a674458127db
Francesca Villalva <pilar.villalva@gmail.com>
2022-04-03 18:40:58 -0400

    add beginner lost without a map challenge solution

---
+++ 8kyu/beginnerLostWithoutAMap.js
@@ -0,0 +1,13 @@
+/*
+8kyu_beginner lost without a map
+27 march 2022
+
+Given an array of integers, return a new array with each value doubled.
+
+Ex:
+[1, 2, 3] --> [2, 4, 6]
+*/
+
+function maps(x){
+    return x.map(x => x * 2)
+}
