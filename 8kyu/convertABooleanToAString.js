2fe81773bd9383ee6d5cf4c5912d9ca54b18f816
Francesca Villalva <pilar.villalva@gmail.com>
2022-04-04 01:16:43 -0400

    add convert a boolean to a string challenge solution

---
+++ 8kyu/convertABooleanToAString.js
@@ -0,0 +1,12 @@
+/*
+8kyu_convert a boolean to a string
+27 march 2022
+
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+*/
+
+function booleanToString(b){
+  return `${b}`
+}
