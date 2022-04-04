99da18bbd78eb7c7ef445dea7dd0aa18b69ef5ed
Francesca Villalva <pilar.villalva@gmail.com>
2022-04-04 01:21:10 -0400

    add convert boolean values to string challenge solution

---
+++ 8kyu/convertBooleanValuesToString.js
@@ -0,0 +1,10 @@
+/*
+8kyu convert boolean values to strings
+23 Mar 2022
+
+Complete a method that takes a boolean value and return a 'Yes' string for true, or a 'No' string for  false
+*/
+
+function boolToWord(bool) {
+    return book ? 'Yes' : 'No'
+}
