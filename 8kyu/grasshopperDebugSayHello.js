0929bd1d54942264de96748f6f99ee4f705533dd
Francesca Villalva <pilar.villalva@gmail.com>
2022-04-07 12:34:29 -0400

    add challenge solution

---
+++ 8kyu/grasshopperDebugSayHello.js
@@ -0,0 +1,12 @@
+/*
+8kyu grasshopper debug sayHello
+6 april 2022
+
+The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+Ex: Hello, Mr. Spock
+*/
+
+function sayHello(name) {
+    return 'Hello, ' + name
+}
