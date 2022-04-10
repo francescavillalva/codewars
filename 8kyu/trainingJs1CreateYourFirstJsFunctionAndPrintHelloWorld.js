889bd7f72fd9b286d69d0dd8556ab5cb5c48980d
Francesca Villalva <pilar.villalva@gmail.com>
2022-04-10 16:45:07 -0400

    add challenge solution

---
+++ 8kyu/trainingJs1CreateYourFirstJsFunctionAndPrintHelloWorld.js
@@ -0,0 +1,18 @@
+/*
+8kyu training js #1 create your first js function and print hello world
+10 april 2022
+
+mission 1:
+use keyword function to define your function, function name should be helloWorld(don't forget the () and {})
+
+mission 2:
+use keyword var to define a variable str, value of str should be a string: "Hello World!"(don't forget the =).
+
+mission 3:
+type the console.log() in the next line(don't forget to put the str in the brackets).
+*/
+
+function helloWorld() {
+  var str = 'Hello World!'
+  console.log( str )
+}
