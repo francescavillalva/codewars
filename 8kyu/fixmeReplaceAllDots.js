8b57f4701cc76c8a128359a8ac26a4e0bdd7ddf4
Francesca Villalva <pilar.villalva@gmail.com>
2022-04-11 15:15:28 -0400

    add challenge solution

---
+++ 8kyu/fixmeReplaceAllDots.js
@@ -0,0 +1,18 @@
+/*
+8kyu fixme replace all dots
+11 april 2022
+
+The code provided is supposed replace all the dots . in the specified String str with dashes -
+
+But it's not working properly.
+
+Task
+Fix the bug so we can all go home early.
+
+Notes
+String str will never be null.
+*/
+
+var replaceDots = function(str) {
+    return str.replace(/\./g, '-');
+}
