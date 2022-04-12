6443a063c88e1da6a00651e2d1b0280fa8511c91
Francesca Villalva <pilar.villalva@gmail.com>
2022-04-12 15:16:30 -0400

    add challenge solution

---
+++ 8kyu/grasshopperFunctionSyntaxDebugging.js
@@ -0,0 +1,10 @@
+/*
+8kyu Grasshopper - Function syntax debugging
+11 april 2022
+
+A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+*/
+
+function main(verb, noun) {
+    return verb + noun
+}
