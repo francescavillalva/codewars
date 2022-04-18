12619dbb3ab4603515ae049ff3400c0df8ff0c06
Francesca Villalva <pilar.villalva@gmail.com>
2022-04-17 22:32:08 -0400

    add challenge solution

---
+++ 8kyu/incorrectDivisionMethod.js
@@ -0,0 +1,8 @@
+/*
+8kyu incorrect division method
+26 april 2022
+
+This method, which is supposed to return the result of dividing its first argument by its second, isn't always returning correct values. Fix it.
+*/
+
+const solve = (x, y) => x / y
