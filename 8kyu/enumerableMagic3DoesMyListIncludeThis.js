032a1c7c1e99e5a7b86264a432df9b25f4d66897
Francesca Villalva <pilar.villalva@gmail.com>
2022-04-18 22:30:36 -0400

    add challenge solution

---
+++ 8kyu/enumerableMagic3DoesMyListIncludeThis.js
@@ -0,0 +1,10 @@
+/*
+8kyu enumerable magic 3 does my list include this
+26 april 2022
+
+Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+*/
+
+function include(arr, item){
+  return arr.includes(item)
+}
