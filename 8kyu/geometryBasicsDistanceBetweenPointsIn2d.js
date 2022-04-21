724fd2a353415ee0822d88a57ceb9eccc291a73b
Francesca Villalva <pilar.villalva@gmail.com>
2022-04-21 19:18:05 -0400

    add challenge solution

---
+++ 8kyu/geometryBasicsDistanceBetweenPointsIn2d.js
@@ -0,0 +1,16 @@
+/*
+8kyu geometry basics distance between points in 2d
+4 may 2022
+
+This series of katas will introduce you to basics of doing geometry with computers.
+
+Point objects have x and y attributes (X and Y in C#) attributes.
+
+Write a function calculating distance between Point a and Point b.
+
+Tests round answers to 6 decimal places.
+*/
+
+function distanceBetweenPoints(a, b) {
+  return Math.sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2)
+}
