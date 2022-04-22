56374aab70088578afc8c0a4f0a6028879208030
Francesca Villalva <pilar.villalva@gmail.com>
2022-04-22 12:16:02 -0400

    add challenge solution

---
+++ 8kyu/grasshopperVariableAssignmentDebug.js
@@ -0,0 +1,11 @@
+/*
+8kyu grasshopper variable assignment debug
+25 april 2022
+
+Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+*/
+
+var a = "dev"
+var b = "Lab"
+
+var name = a + b
