158cae5430b6f4e95b57889f82623ab181c1d1ae
Francesca Villalva <pilar.villalva@gmail.com>
2022-04-23 19:18:50 -0400

    add challenge solution

---
+++ 8kyu/duckDuckGoose.js
@@ -0,0 +1,10 @@
+/*
+8kyu duck duck goose
+5 may 2022
+
+Task: Given an array of Player objects (an array of associative arrays in PHP) and an index (1-based), return the name of the chosen Player(name is a property of Player objects, e.g Player.name)
+*/
+
+function duckDuckGoose(players, goose) {
+    return players[(goose - 1) % players.length].name
+}
