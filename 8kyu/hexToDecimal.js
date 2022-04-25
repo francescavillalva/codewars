5636621b36be73ad8c8d8000983a1304f1c695f4
Francesca Villalva <pilar.villalva@gmail.com>
2022-04-25 16:32:43 -0400

    add challenge solution

---
+++ 8kyu/hexToDecimal.js
@@ -0,0 +1,10 @@
+/*
+8kyu hex to decimal
+25 april 2022
+
+Complete the function which converts hex number (given as a string) to a decimal number.
+*/
+
+function hexToDec(hexString){
+  return parseInt(hexString, 16)
+}
