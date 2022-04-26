799e97bb3ecbd95382b4337796a3598ddabd2060
Francesca Villalva <pilar.villalva@gmail.com>
2022-04-26 12:14:21 -0400

    add challenge solution

---
+++ 8kyu/binToDecimal.js
@@ -0,0 +1,10 @@
+/*
+8kyu bin to decimal
+25 april 2022
+
+Complete the function which converts a binary number (given as a string) to a decimal number.
+*/
+
+function binToDec(bin){
+  return parseInt(bin, 2)
+}
