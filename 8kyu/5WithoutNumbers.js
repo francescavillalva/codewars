1f7d36a2f0d5fb637e1f3006b2a77b93720d47f5
Francesca Villalva <pilar.villalva@gmail.com>
2022-04-26 12:13:59 -0400

    add challenge solution

---
+++ 8kyu/5WithoutNumbers.js
@@ -0,0 +1,12 @@
+/*
+8kyu 5 without numbers
+25 april 2022
+
+Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+*/
+
+function unusualFive() {
+  return 'apple'.length
+}
