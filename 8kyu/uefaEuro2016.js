60033f8cc1fe85b4e9d0e5cc02e9aa2ac6ebd5e5
Francesca Villalva <pilar.villalva@gmail.com>
2022-05-08 23:26:12 -0400

    add challenge solution

---
+++ 8kyu/uefaEuro2016.js
@@ -0,0 +1,22 @@
+/*
+8kyu eufa euro 2016
+8 may 2022
+
+Finish the uefaEuro2016() function so it return string just like in the examples below:
+
+uefaEuro2016(['Germany', 'Ukraine'],[2, 0]) // "At match Germany - Ukraine, Germany won!"
+uefaEuro2016(['Belgium', 'Italy'],[0, 2]) // "At match Belgium - Italy, Italy won!"
+uefaEuro2016(['Portugal', 'Iceland'],[1, 1]) // "At match Portugal - Iceland, teams played draw."
+*/
+
+function uefaEuro2016(teams, scores){
+  let outcome = ''
+  if (scores[0] === scores[1]) {
+    outcome = 'teams played draw.'
+  } else if (scores[0] > scores[1]) {
+    outcome = `${teams[0]} won!`
+  } else {
+    outcome = `${teams[1]} won!`
+  }
+  return `At match ${teams.join(' - ')}, ${outcome}`
+}
