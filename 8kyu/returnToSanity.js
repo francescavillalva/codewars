008a9d52789f2a9155b47dd58b3e1697e61bbab9
Francesca Villalva <pilar.villalva@gmail.com>
2022-05-08 23:25:52 -0400

    add challenge solution

---
+++ 8kyu/returnToSanity.js
@@ -0,0 +1,12 @@
+/*
+8kyu return to sanity
+8 may 2022
+
+This function should return an object, but it's not doing what's intended. What's wrong?
+*/
+
+function mystery() {
+  var results =
+    {sanity: 'Hello'};
+  return results;
+}
