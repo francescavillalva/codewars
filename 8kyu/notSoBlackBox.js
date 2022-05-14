a92e6f71d7416638140fd7bb885929b0a88cf581
Francesca Villalva <pilar.villalva@gmail.com>
2022-05-13 22:33:50 -0400

    add challenge solution

---
+++ 8kyu/notSoBlackBox.js
@@ -0,0 +1,12 @@
+/*
+8kyu not so black box
+12 may 2022
+
+The kata has been preloaded with a function called blackBox. Figure out what the function does.
+
+You may need to think laterally!
+*/
+
+blackBox()
+
+JSopenSesame()
