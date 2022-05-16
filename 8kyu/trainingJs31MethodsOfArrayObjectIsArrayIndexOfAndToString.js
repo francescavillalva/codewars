c1530550ed2152842495ca4cabea611dc55da2e1
Francesca Villalva <pilar.villalva@gmail.com>
2022-05-15 22:32:09 -0400

    add challenge solution

---
+++ 8kyu/trainingJs31MethodsOfArrayObjectIsArrayIndexOfAndToString.js
@@ -0,0 +1,28 @@
+/*
+8kyu training js 31 methods of array object - isArray, indexOf, and toString
+12 may 2022
+
+Coding in function blackAndWhite. function accept 1 parameter arr(a number array).
+
+If arr is not an array, function should return:
+
+"It's a fake array"
+If arr contains elements 5 and 13, function should return:
+
+"It's a black array"
+If arr contains neither 5 nor 13, function should return:
+
+"It's a white array"
+Examples
+
+blackAndWhite(5,13) should return "It's a fake array"
+blackAndWhite([5,13]) should return "It's a black array"
+blackAndWhite([5,12]) should return "It's a white array" 
+Using string template and ternary operator can make your work easier.
+*/
+
+function blackAndWhite(arr){
+  if (!Array.isArray(arr)) return 'It\'s a fake array'
+  if (arr.indexOf(5) !== -1 && arr.indexOf(13) !== -1) return 'It\'s a black array'
+  return 'It\'s a white array'
+}
